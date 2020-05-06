787e62c58d284e02f95383a19efbe70a57ff02a0
Wisnu <wisnu@gositus.com>
2020-05-07 00:34:35 +0700

    Updates

---
+++ precache-manifest.b8c02e01cb73876c3559fde8f635616e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8624d65e6d6db1e2fb65b418b91ca3e2",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "53034d8799c78daab2f8",
+    "url": "/resume/static/css/main.c5875702.chunk.css"
+  },
+  {
+    "revision": "d59e8d3b32a409733942",
+    "url": "/resume/static/js/2.6fcf2d16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume/static/js/2.6fcf2d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53034d8799c78daab2f8",
+    "url": "/resume/static/js/main.8f505af0.chunk.js"
+  },
+  {
+    "revision": "80b6511a4fe8944e422b",
+    "url": "/resume/static/js/runtime-main.3ebc219f.js"
+  }
+]);
